0429e7b4fb3e90bb460e3fb9bb7ecb9994e971d2
vesprada <vesprada@localhost>
2015-02-19 20:19:27 +0100

    comit

---
+++ src/ed_ejercicios/ED_Ejercicios.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ed_ejercicios;
+
+/**
+ *
+ * @author vesprada
+ */
+import java.util.Scanner;
+import java.io.*;
+public class ED_Ejercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String ruta;
+        Scanner Sc = new Scanner(System.in);
+        int cont=0;
+        try{
+            System.out.print("Introduce la ruta del archivo: ");      
+            ruta = Sc.nextLine();
+            FileReader fr=new FileReader(ruta);
+            int valor=fr.read();
+            while(valor !=-1){
+                System.out.print((char)valor);
+                valor=fr.read();
+                   cont++;
+            }
+                fr.close();
+                System.out.println("Numero de caracteres que contiene el archivo: "+cont);
+        }catch(IOException e){
+            System.out.println("Error E/S: "+e);
+        }
+        }
+    }
+    
